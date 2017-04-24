e1ca37deda4c17c447f001ae9bdc1a45ec3d2775
Jacob Chen <jacob-chen@iotwrt.com>
2017-04-24 09:39:37 +0800

    format shell script
    Change-Id: I43735e8b0f922c96cf42e73d602f309db357202a
    Signed-off-by: Jacob Chen <jacob-chen@iotwrt.com>

--- flash_tool.sh
+++ flash_tool.sh
@@ -9,12 +9,12 @@ BOOT_SIZE=229376
 
 SYSTEM_START=0
 LOADER1_START=64
-RESERVED1_START=`expr ${LOADER1_START}  + ${LOADER1_SIZE}`
-RESERVED2_START=`expr ${RESERVED1_START}  + ${RESERVED1_SIZE}`
-LOADER2_START=`expr ${RESERVED2_START}  + ${RESERVED2_SIZE}`
-ATF_START=`expr ${LOADER2_START}  + ${LOADER2_SIZE}`
-BOOT_START=`expr ${ATF_START}  + ${ATF_SIZE}`
-ROOTFS_START=`expr ${BOOT_START}  + ${BOOT_SIZE}`
+RESERVED1_START=$(expr ${LOADER1_START} + ${LOADER1_SIZE})
+RESERVED2_START=$(expr ${RESERVED1_START} + ${RESERVED1_SIZE})
+LOADER2_START=$(expr ${RESERVED2_START} + ${RESERVED2_SIZE})
+ATF_START=$(expr ${LOADER2_START} + ${LOADER2_SIZE})
+BOOT_START=$(expr ${ATF_START} + ${ATF_SIZE})
+ROOTFS_START=$(expr ${BOOT_START} + ${BOOT_SIZE})
 
 LOCALPATH=$(pwd)
 TOOLPATH=${LOCALPATH}/rkbin/tools
@@ -26,7 +26,7 @@ SEEK="0"
 
 usage() {
 	echo -e "\nUsage: rk3x \n"
-	echo -e	"		emmc: build/flash_tool.sh -c rk3288  -p system -i out/system.img  \n"
+	echo -e "		emmc: build/flash_tool.sh -c rk3288  -p system -i out/system.img  \n"
 	echo -e "       sdcard: build/flash_tool.sh -c rk3288  -d /dev/sdb -p system  -i out/system.img \n"
 	echo -e "\nUsage: rv \n"
 	echo -e "       sdcard: build/flash_tool.sh -c rk1108 -i out/firmware.img \n"
@@ -39,8 +39,7 @@ finish() {
 }
 trap finish ERR
 
-while getopts "c:t:s:d:p:r:d:i:h" flag
-do
+while getopts "c:t:s:d:p:r:d:i:h" flag; do
 	case $flag in
 		c)
 			CHIP="$OPTARG"
@@ -50,7 +49,7 @@ do
 			;;
 		i)
 			IMAGE="$OPTARG"
-			if [ ! -e ${IMAGE} ] ; then
+			if [ ! -e ${IMAGE} ]; then
 				echo -e "\e[31m CAN'T FIND IMAGE \e[0m"
 				usage
 				exit
@@ -58,37 +57,36 @@ do
 			;;
 		p)
 			PARTITIONS="$OPTARG"
-			BPARTITIONS=`echo $PARTITIONS| tr 'a-z' 'A-Z'`
+			BPARTITIONS=$(echo $PARTITIONS | tr 'a-z' 'A-Z')
 			SEEK=${BPARTITIONS}_START
 			eval SEEK=\$$SEEK
 
-			if [ -n "$(echo $SEEK| sed -n "/^[0-9]\+$/p")" ];then  
-				echo "PARTITIONS OFFSET: $SEEK sectors."  
-			else  
+			if [ -n "$(echo $SEEK | sed -n "/^[0-9]\+$/p")" ]; then
+				echo "PARTITIONS OFFSET: $SEEK sectors."
+			else
 				echo -e "\e[31m INVAILD PARTITION.\e[0m"
 				exit
-			fi  
+			fi
 			;;
 	esac
 done
 
-if [ ! $IMAGE ] ; then
+if [ ! $IMAGE ]; then
 	usage
 	exit
 fi
 
-flash_upgt() 
-{
-	if [ "${CHIP}" == "rk3288" ] ; then
-		sudo $TOOLPATH/rkdeveloptool db  ${LOCALPATH}/rkbin/rk32/rk3288_ubootloader_v1.01.06.bin
-	elif [ "${CHIP}" == "rk3036" ] ; then
-		sudo $TOOLPATH/rkdeveloptool db  ${LOCALPATH}/rkbin/rk30/rk3036_loader_v1.07.219.bin
-	elif [ "${CHIP}" == "rk3399" ] ; then
-		sudo $TOOLPATH/rkdeveloptool db  ${LOCALPATH}/rkbin/rk33/rk3399_loader_v1.08.106.bin
-	elif [ "${CHIP}" == "rk3228" ] ; then
-		sudo $TOOLPATH/rkdeveloptool db  ${LOCALPATH}/rkbin/rk33/rk3328_loader_v1.00.238.bin
-	elif [ "${CHIP}" == "rv1108" ] ; then
-		sudo $TOOLPATH/rkdeveloptool db  ${LOCALPATH}/rkbin/rv1x/RK1108_usb_boot.bin
+flash_upgt() {
+	if [ "${CHIP}" == "rk3288" ]; then
+		sudo $TOOLPATH/rkdeveloptool db ${LOCALPATH}/rkbin/rk32/rk3288_ubootloader_v1.01.06.bin
+	elif [ "${CHIP}" == "rk3036" ]; then
+		sudo $TOOLPATH/rkdeveloptool db ${LOCALPATH}/rkbin/rk30/rk3036_loader_v1.07.219.bin
+	elif [ "${CHIP}" == "rk3399" ]; then
+		sudo $TOOLPATH/rkdeveloptool db ${LOCALPATH}/rkbin/rk33/rk3399_loader_v1.08.106.bin
+	elif [ "${CHIP}" == "rk3228" ]; then
+		sudo $TOOLPATH/rkdeveloptool db ${LOCALPATH}/rkbin/rk33/rk3328_loader_v1.00.238.bin
+	elif [ "${CHIP}" == "rv1108" ]; then
+		sudo $TOOLPATH/rkdeveloptool db ${LOCALPATH}/rkbin/rv1x/RK1108_usb_boot.bin
 	fi
 
 	sudo $TOOLPATH/rkdeveloptool wl ${SEEK} ${IMAGE}
@@ -98,12 +96,11 @@ flash_upgt()
 	sudo $TOOLPATH/rkdeveloptool rd
 }
 
-flash_sdcard() 
-{
+flash_sdcard() {
 	sudo dd if=${IMAGE} of=${DEVICE} seek=${SEEK} conv=notrunc
 }
 
-if [ ! $DEVICE ] ; then
+if [ ! $DEVICE ]; then
 	flash_upgt
 else
 	flash_sdcard
